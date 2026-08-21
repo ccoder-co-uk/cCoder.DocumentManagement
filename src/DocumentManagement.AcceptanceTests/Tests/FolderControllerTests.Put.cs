f5d42401992421c6598e5b4e0c40f0adada52498
TehWardy <paul.ward@ccoder.co.uk>
2026-08-21 08:53:41 +0100

    test: cover folder parent updates

--- src/DocumentManagement.AcceptanceTests/Tests/FolderControllerTests.Put.cs
+++ src/DocumentManagement.AcceptanceTests/Tests/FolderControllerTests.Put.cs
@@ -48,4 +48,47 @@ public sealed partial class FolderControllerTests
         await DeleteFolderAsync(id: createdFolder.Id);
         await Teardown(seededContext: seededContext);
     }
+
+    [Fact]
+    public async Task Put_MovesFolderToParent()
+    {
+        // Given
+        SeededFolderContext seededContext = await SeedDatabase(
+            privileges: ["folder_create", "folder_update", "folder_delete"]);
+
+        Folder sourceFolder = await CreateFolderAsync(payload: new
+        {
+            appId = seededContext.AppId,
+            name = Unique(prefix: "SourceFolder"),
+            path = "sourcefolder",
+        });
+
+        Folder targetFolder = await CreateFolderAsync(payload: new
+        {
+            appId = seededContext.AppId,
+            name = Unique(prefix: "TargetFolder"),
+            path = "targetfolder",
+        });
+
+        // When
+        await UpdateFolderAsync(id: sourceFolder.Id, payload: new
+        {
+            id = sourceFolder.Id,
+            appId = seededContext.AppId,
+            name = sourceFolder.Name,
+            parentId = targetFolder.Id,
+            path = $"{targetFolder.Path}/{sourceFolder.Name.ToLowerInvariant()}",
+        });
+
+        Folder actualFolder = await GetFolderAsync(id: sourceFolder.Id);
+
+        // Then
+        actualFolder.Should()
+            .NotBeNull();
+
+        actualFolder!.ParentId.Should()
+            .Be(expected: targetFolder.Id);
+
+        await Teardown(seededContext: seededContext);
+    }
 }
